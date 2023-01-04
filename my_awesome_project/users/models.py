217dda71cc536eb1c7d60dc6a8cb9c792dd1f320
Pamela Fox <pamela.fox@gmail.com>
2023-01-04 14:26:20 -0800

    Add image field to user admin

--- my_awesome_project/users/models.py
+++ my_awesome_project/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, ImageField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -15,6 +15,7 @@ class User(AbstractUser):
     name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+    image = ImageField(blank=True)
 
     def get_absolute_url(self):
         """Get url for user's detail view.
